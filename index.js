1895d714958d0f28957a6d35c6ee6ff09f76ec0d
Cody Lawdermilt <lawdermiltcody@gmail.com>
2019-04-18 17:31:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,10 +29,9 @@ function iLoveTheBeatles(n){
     n++;
   } while (n < 15);
   return love;
- 
+}
 
-  let arr = []
-
+  var arr = [];
   for (let i = 0, l = musicians.length; i < l; i++) {
     arr.push(`musicians[i] + "plays" + instruments[i]`)
   }
