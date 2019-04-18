725a7f714c6d4fc84eae2c669ed42301f7450109
Cody Lawdermilt <lawdermiltcody@gmail.com>
2019-04-18 17:34:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts(facts){
   let shout = [];
   
   let i = 0;
-  while(i< facts.length){
+  while(i < facts.length){
     console.log(facts[i] + "!!!");
     i++
   }
@@ -29,16 +29,7 @@ function iLoveTheBeatles(n){
     n++;
   } while (n < 15);
   return love;
- 
-
-  var arr = [
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`musicians[i] + "plays" + instruments[i]`)
-  }
-
-  return arr
-}
-
+ }
 
 function johnLennonFacts(facts) {
   const shoutedFacts = []
